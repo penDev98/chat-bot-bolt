4e28ab98d39ce93764aebaab5ca3b8b48b0ff2b8
Kerim Pendev <kerim.pendev@egt-interactive.com>
2026-03-13 16:46:18 +0200

    fixxxx

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,6 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      '/api/external': {
-        target: 'https://100imoti-fe.vercel.app',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api\/external/, '/api'),
-        secure: false,
-      }
-    }
-  },
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
